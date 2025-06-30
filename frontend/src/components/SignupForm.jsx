ad4bc45849dacc1e71f3b6e74202a5f9ca76d9d1
Prince Singh <princesingh6173@gmail.com>
2025-06-30 15:01:26 +0530

    Update SignupForm.jsx

--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -18,7 +18,7 @@ const SignupForm = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/signup", {
+      const res = await fetch("https://cinescope-backend-n7jh.onrender.com/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
